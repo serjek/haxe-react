ce7935881c7566bb88b5dc208a55b0d0d9cc23b2
k <k@klabz.org>
2018-08-24 12:25:28 +0200

    Remove Fragment warning for react < 16.2
    Can cause errors in some rare cases with webpack.
    It's not the only react 16.2+ feature present here anymore, anyway

--- src/lib/react/Fragment.hx
+++ src/lib/react/Fragment.hx
@@ -5,16 +5,5 @@ package react;
 	https://reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
 **/
 @:native('React.Fragment')
-extern class Fragment extends ReactComponent {
-	#if debug
-	static function __init__():Void
-	{
-		var version = react.React.version.split('.')
-			.filter(function(s) return s != '.')
-			.map(untyped parseInt);
-		
-		if (version[0] < 16 || version[0] == 16 && version[1] < 2)
-			js.Browser.console.warn('Fragments are not available on react < 16.2.0');
-	}
-	#end
-}
+extern class Fragment extends ReactComponent {}
+
