22b64ddbb8ddfca7b9d5bd9544780580ff6dbaa3
k <k@klabz.org>
2018-09-24 15:08:42 +0200

    Release version 1.100.1

--- releaseHaxelib.sh
+++ releaseHaxelib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f haxe-react.zip
-zip -r haxe-react.zip src haxelib.json README.md changes.md doc
+zip -r haxe-react.zip src haxelib.json README.md changes.md doc extraParams.hxml
 # haxelib submit haxe-react.zip $1 $2 --always
 haxelib submit haxe-react.zip
