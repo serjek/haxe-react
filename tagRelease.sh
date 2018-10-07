11a60a1cfa9feafb2474704d93e2958dcb077100
k <k@klabz.org>
2018-10-07 10:40:07 +0200

    1.101.0 - ReactComponent builders with keys

--- tagRelease.sh
+++ tagRelease.sh
@@ -23,21 +23,25 @@ fi
 # Change version in haxelib.json and package.json
 echo "Update version to $VERSION"
 PATTERN="s/\"version\": \"[0-9]+.[0-9]+.[0-9]+\"/\"version\": \"$VERSION\"/g"
+EXTRA_PATTERN="s/-D react=[0-9]+.[0-9]+.[0-9]+/-D react=$VERSION/g"
 
 case "$(uname -s)" in
    Darwin)
      sed -E -i "" "$PATTERN" ./haxelib.json
      sed -E -i "" "$PATTERN" ./mdk/info.json
+     sed -E -i "" "$EXTRA_PATTERN" ./extraParams.hxml
      ;;
    *)
      sed -E -i "$PATTERN" ./haxelib.json
      sed -E -i "$PATTERN" ./mdk/info.json
+     sed -E -i "$EXTRA_PATTERN" ./extraParams.hxml
      ;;
 esac
 
 # Tag, commit and push to trigger a new CI release
 git add ./haxelib.json
 git add ./mdk/info.json
+git add ./extraParams.hxml
 git commit -m "$VERSION$MESSAGE"
 git push fork next
 git tag $VERSION
