7e17334e134e195da63e58ae7488f4e887c3e11e
k <k@klabz.org>
2018-12-04 11:20:37 +0100

    Added ReactMacro.getExtraProps()

--- src/lib/react/ReactMacro.hx
+++ src/lib/react/ReactMacro.hx
@@ -54,6 +54,29 @@ class ReactMacro
 		};
 	}
 
+	public static macro function getExtraProps(props:Expr):Expr {
+		return switch (Context.typeof(props)) {
+			case _ => Context.followWithAbstracts(_) => TypeTools.toComplexType(_) => TAnonymous(fields):
+				macro {
+					var _tmp = js.Object.assign({}, ${props});
+
+					$a{fields.map(function(f) {
+						return macro untyped __js__('delete {0}[{1}]', _tmp, $v{f.name});
+					})}
+
+					_tmp;
+				};
+
+			default:
+				Context.fatalError(
+					'getExtraProps() expects an anonymous structure as type of argument',
+					Context.currentPos()
+				);
+
+				macro {};
+		};
+	}
+
 	#if macro
 	static var REACT_FRAGMENT_CT = macro :react.ReactComponent.ReactFragment;
 
