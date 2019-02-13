43e130369a1b8d5834d7a099852c4550ea7a5afa
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2019-02-13 11:57:40 +0100

    Fix Fragment when not using react global

--- src/lib/react/Fragment.hx
+++ src/lib/react/Fragment.hx
@@ -6,6 +6,9 @@ import react.ReactComponent;
 	Warning: Fragments are only available in react 16.2.0+
 	https://reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
 **/
+#if (!react_global)
+@:jsRequire("react", "Fragment")
+#end
 @:native('React.Fragment')
 extern class Fragment extends ReactComponentOfProps<Empty> {}
 
